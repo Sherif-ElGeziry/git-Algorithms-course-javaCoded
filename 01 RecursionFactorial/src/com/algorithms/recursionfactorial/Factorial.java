1436595c6721139b0a36b8ff16a5cd92b6543886
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-11 20:54:55 +0200

    Factorial done using recursion

---
+++ 01 RecursionFactorial/src/com/algorithms/recursionfactorial/Factorial.java
@@ -0,0 +1,22 @@
+package com.algorithms.recursionfactorial;
+
+public class Factorial {
+	int value;
+
+	public int FactorialUsingRecursion(int value) {
+		if (value == 1) {
+			return value;
+		}
+
+		return value * FactorialUsingRecursion(value - 1);
+	}
+
+	public int FactorialUsingIterative(int value) {
+		int total = 1;
+
+		for (int i = 2; i <= value; i++) {
+			total *= i;
+		}
+		return total;
+	}
+}
--- 01 RecursionFactorial/src/com/algorithms/recursionfactorial/Main.java
+++ 01 RecursionFactorial/src/com/algorithms/recursionfactorial/Main.java
@@ -3,8 +3,19 @@ package com.algorithms.recursionfactorial;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Factorial fac1 = new Factorial();
 
+		System.out.println(fac1.FactorialUsingRecursion(5));
+		System.out.println(fac1.FactorialUsingIterative(5));
+
+		System.out.println(fac1.FactorialUsingRecursion(9));
+		System.out.println(fac1.FactorialUsingIterative(9));
+
+		System.out.println(fac1.FactorialUsingRecursion(2));
+		System.out.println(fac1.FactorialUsingIterative(2));
+
+		System.out.println(fac1.FactorialUsingRecursion(1));
+		System.out.println(fac1.FactorialUsingIterative(1));
 	}
 
 }
