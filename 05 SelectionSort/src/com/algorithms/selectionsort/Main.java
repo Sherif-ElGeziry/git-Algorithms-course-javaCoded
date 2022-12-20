e360631f306098aa6eab35bf6a529cab29aec767
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-20 19:18:07 +0200

    Implemented sorting using selection sort

--- 05 SelectionSort/src/com/algorithms/selectionsort/Main.java
+++ 05 SelectionSort/src/com/algorithms/selectionsort/Main.java
@@ -7,11 +7,11 @@ public class Main {
 	public static void main(String[] args) {
 		Integer[] array = { 6, 2, 1, 5, 63, 87, 283, 4, 0 };
 
-		SelectionSort bub1 = new SelectionSort(array);
+		SelectionSort sel1 = new SelectionSort(array);
 
 		// below two different ways of printing output
-		Arrays.stream(bub1.bubbleSort()).forEach(System.out::println);
-		System.out.println(Arrays.toString(bub1.bubbleSort()));
+		Arrays.stream(sel1.bubbleSort()).forEach(System.out::println);
+		System.out.println(Arrays.toString(sel1.bubbleSort()));
 
 	}
 
